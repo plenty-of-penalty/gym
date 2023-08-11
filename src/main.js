bd6da81853e82b2674d050c6251dffb70db2bdd7
memset0 (ChromeOS) <memset0@outlook.com>
2023-08-12 00:38:38 +0800

    init node

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
 const path = require("path");
+const YAML = require("yaml");
 
 const collections = {};
-const contests = {};
+const contests = [];
+
+const content_dir = path.join(__dirname, "../content");
+
+for (const collection_filename of fs.readdirSync(content_dir)) {
+    const collection_filedir = path.join(content_dir, collection_filename);
+    const collection = YAML.parse(fs.readFileSync(collection_filedir).toString());
+    console.log(collection);
+}
